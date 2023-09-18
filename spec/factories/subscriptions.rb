cd48eafe83c850bf9f6c138f7a69e3048005b3d5
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-18 15:11:59 -0600

    chore: model factories

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :subscription do
-    title { "MyString" }
-    price { 1.5 }
-    status { "MyString" }
-    frequency { "MyString" }
+    title { ["Monthly", "Yearly"].sample + " " + Faker::Tea.variety + " Box" }
+    price { Faker::Commerce.price(range: 10..30.0) }
+    status { ["active", "cancelled"].sample }
+    frequency { ["monthly", "yearly"].sample }
+    customer
   end
 end
