beaf89fb5b38ef59a3f7d35c7510cd1c25c1f8b3
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-18 14:37:07 -0600

    test: customer#index

---
+++ spec/requests/api/v1/customers_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+require 'factory_bot_rails'
+
+
+RSpec.describe "/customers", type: :request do
+
+  describe "GET /index" do
+    it "renders a successful response" do
+      get api_v1_customers_path
+      customer_info = JSON.parse(response.body, symbolize_names: true)
+      customer = customer_info[:data]
+
+      customer.each do |customer|
+        expect(customer[:data]).to have_key(:first_name)
+        expect(customer[:data][:first_name]).to be_a(String)
+        expect(customer[:data]).to have_key(:last_name)
+        expect(customer[:data][:last_name]).to be_a(String)
+        expect(customer[:data]).to have_key(:email)
+        expect(customer[:data][:email]).to be_a(String)
+        expect(customer[:data]).to have_key(:address)
+        expect(customer[:data][:address]).to be_a(text)
+      end
+    end
+  end
+
+
+  # describe "GET /show" do
+  #   it "renders a successful response" do
+  #     customer = Customer.create! valid_attributes
+  #     get customer_url(customer), as: :json
+  #     expect(response).to be_successful
+  #   end
+  # end
+end
