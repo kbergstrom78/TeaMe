af165b7e5290819f1b69e14db053123e492d1af4
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-19 14:50:18 -0600

    refact: add validations to Customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
   has_many :teas, through: :subscriptions
+
+  validates_presence_of :first_name, :last_name, :email, :address
+  validates_uniqueness_of :email, case_sensitive: false
 end
