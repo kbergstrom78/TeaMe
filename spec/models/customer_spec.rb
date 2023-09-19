92f3a3dcc8976f8462e196d10bda74290c5e818f
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-19 14:50:34 -0600

    test: add validation tests to Customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,5 +4,12 @@ RSpec.describe Customer, type: :model do
   describe "relationships" do
     it { should have_many(:subscriptions) }
     it { should have_many(:teas).through(:subscriptions)}
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value("user@example.com").for(:email) }
+    it { should validate_presence_of(:address) }
   end
 end
