5bc21566022c7d418f5d2d4cb294fdf35a8185c0
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-09-21 13:50:18 -0600

    refact: lean out controller

--- app/controllers/api/v1/customers/subscriptions_controller.rb
+++ app/controllers/api/v1/customers/subscriptions_controller.rb
@@ -5,7 +5,6 @@ module Api
         before_action :set_customer
 
         def create
-          # require 'pry'; binding.pry
           tea = Tea.find_by_id(params[:tea_id])
           return render json: { error: 'Tea not found' }, status: :not_found unless tea
 
@@ -21,22 +20,15 @@ module Api
 
         def update
           @subscription = @customer.subscriptions.find_by_id(params[:id])
+          return render json: { error: 'Subscription not found' }, status: :not_found unless @subscription
 
-          if @subscription
-            if @subscription.active? && subscription_params[:status] == 'cancelled'
-              if @subscription.update(status: 'cancelled')
-                render json: { message: 'Subscription cancelled successfully' }, status: :ok
-              else
-                render json: { error: 'Unable to cancel subscription' }, status: :unprocessable_entity
-              end
-            elsif !@subscription.active? && subscription_params[:status] == 'cancelled'
-              render json: { error: 'Only active subscriptions can be canceled' }, status: :unprocessable_entity
-            else
-              render json: { error: 'Invalid status update' }, status: :unprocessable_entity
-            end
+          if subscription_params[:status] == 'cancelled'
+            handle_cancel_subscription and return
+          else
+            render json: { error: 'Invalid status update' }, status: :unprocessable_entity and return
           end
         end
-
+        
         def index
           subscriptions = @customer.subscriptions
           render json: SubscriptionSerializer.new(subscriptions).serializable_hash.to_json
@@ -46,6 +38,14 @@ module Api
 
         def set_customer
           @customer = Customer.find(params[:customer_id])
+        end
+
+        def handle_cancel_subscription
+          if @subscription.cancel
+            render json: { message: 'Subscription cancelled successfully' }, status: :ok
+          else
+            render json: { error: 'Unable to cancel subscription' }, status: :unprocessable_entity
+          end
         end
 
         def subscription_params
